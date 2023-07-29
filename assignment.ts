3cc136f27a405ec37c65b57550f372b38c8f7171
muzammal57 <114586464+muzammal57@users.noreply.github.com>
2023-07-29 23:21:34 +0500

    Add files via upload

---
+++ assignment.ts
@@ -0,0 +1,26 @@
+var msg:string="Hey Abu Huraira i have started learning typescript";
+var myname:string="I am Muzammal Hussain";
+console.log(msg);
+console.log(myname);
+var num1:number=1;
+var num2:number=2;var num3:number=3
+var num4:number=4;var num5:number=5;
+var num6:number=6;var num7:number=7;
+var num8:number=8;var num9:number=9
+var num10:number=10
+var sum:number=num1+num2+num3+num4+num5+num6+num7+num8+num9+num10;
+console.log(sum);
+var product:number=num1*num2*num3*num4*num5*num6*num7*num8*num9*num10;
+console.log(product);
+var diff:number=product - sum;
+console.log(diff);
+var div:number=num10/num5;
+console.log(div);
+var allop1:number=num1-num4*num6+num10;
+console.log(allop1);
+var allop2:number=num2-num4*num6+num8;
+var allop3:number=num1-num3*num5+num7;
+var allop4:number=num3-num6*num6+num10;
+console.log(allop2);
+console.log(allop3);
+console.log(allop4);
